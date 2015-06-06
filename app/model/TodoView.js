858cf333b67a0f8f2f2cd92128c2ddaa1976fd97
Corrado Alesso <co.alesso@fastwebnet.it>
2015-06-06 16:12:51 +0200

    Introdotto _.template, sostituito template di TodoView con checkbox

--- app/model/TodoView.js
+++ app/model/TodoView.js
@@ -1,13 +1,14 @@
 TodoView = Backbone.View.extend({
 
     tagName: 'li',
+    template: _.template($('#todoTemplate').text()),
 
     initialize: function() {
         log('TodoView created');
     },
 
     render: function() {
-        this.$el.text(this.model.get('title'));
+        this.$el.html(this.template(this.model.attributes));
         return this;
     }
 
