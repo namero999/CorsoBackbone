7f94ee185d3b966297d29ac3b6fcab5a1aa125e8
Corrado Alesso <co.alesso@fastwebnet.it>
2015-06-20 14:51:02 +0200

    Edit funzionante con reindirizzamento indietro

--- app/model/TodoEditView.js
+++ app/model/TodoEditView.js
@@ -7,6 +7,17 @@ TodoEditView = Backbone.View.extend({
         log('TodoEditView initialize');
     },
 
+    events: {
+        'keypress': 'onEnter'
+    },
+
+    onEnter: function(event) {
+        if (event.keyCode == 13) {
+            this.model.set('title', this.$el.val());
+            router.navigate('', {trigger: true});
+        }
+    },
+
     render: function() {
         log('TodoEditView render');
         this.$el.val(this.model.attributes.title);
